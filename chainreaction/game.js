d69e3279ad50d4929323f53924e167d10cdc0190
Hristo Stoyanov <renegat96@gmail.com>
2013-11-26 18:36:48 +0200

    Добавихме избухване и undo

--- chainreaction/game.js
+++ chainreaction/game.js
@@ -10,6 +10,7 @@ canvas.width = 600;
 canvas.height = 400;
 var context = canvas.getContext("2d");
 var currentPlayer = 0;
+var mishkaX, mishkaY;
 ///NE BARAITE REDOVETE NAGORE!
 
 var cvetove = ["red", "green"];
@@ -28,6 +29,8 @@ function create2d(n, m) {
 var map = create2d(10, 10);
 var whoseBallsAreThese = create2d(10, 10);
 var neighbors = create2d(10, 10);
+var oldmap = create2d(10, 10);
+var oldWhoseBalls = create2d(10, 10);
 
 for (var i = 1; i < 9; ++i) {
     for (var j = 1; j < 9; j++) {
@@ -45,8 +48,25 @@ neighbors[9][0] = 2;
 neighbors[0][9] = 2;
 neighbors[9][9] = 2;
 
+var napravilLiSumUndo = false;
+function undo() {
+    if (!napravilLiSumUndo) {
+        for (var i = 0; i < 10; i++) {
+            for (var j = 0; j < 10; j++) {
+                map[i][j] = oldmap[i][j];
+                whoseBallsAreThese[i][j] = oldWhoseBalls[i][j];
+            }
+        }
+        currentPlayer = 1 - currentPlayer;
+        napravilLiSumUndo = true;
+    }
+}
+
 window.addEventListener("keydown", function (args) {	//Vika se pri natiskane na kopche
     if(!rilizvamLi){console.log(args.keyCode);}  		//pechatame koda na natisnatoto kopche
+    if (args.keyCode == 85) { // 'u'
+        undo();
+    }
 }, false);
 
 window.addEventListener("keyup", function (args) {	//vika se pri puskane na kopche natiskano do sega
@@ -68,14 +88,48 @@ function isThisPointInTheSquare(pointX, pointY,
 }
 
 function explode(i, j) {
-    //TO DO: напишете кода на тази функция, която да „експоаТира“ топчетата!
+    map[i][j] = map[i][j] - neighbors[i][j];
+    if (0 <= i-1) {
+        map[i-1][j]++;
+        whoseBallsAreThese[i-1][j] = whoseBallsAreThese[i][j];
+    }
+    if (j+1 <= 9) {
+        map[i][j+1]++;
+        whoseBallsAreThese[i][j+1] = whoseBallsAreThese[i][j];
+    }
+    if (0 <= j - 1) {
+        map[i][j - 1]++;
+        whoseBallsAreThese[i][j-1] = whoseBallsAreThese[i][j];
+    }
+    if (i+1 <= 9) {
+        map[i+1][j]++;
+        whoseBallsAreThese[i+1][j] = whoseBallsAreThese[i][j];
+    }
+    if (0 <= i - 1 && map[i-1][j] >= neighbors[i-1][j]) {
+        explode(i-1, j);
+    }
+    if (j+1 <= 9 && map[i][j+1] >= neighbors[i][j+1]) {
+        explode(i, j+1);
+    }
+    if (0 <= j-1 && map[i][j-1] >= neighbors[i][j-1]) {
+        explode(i, j-1);
+    }
+    if (i+1 <= 9 && map[i+1][j] >= neighbors[i+1][j]) {
+        explode(i+1, j);
+    }
 }
-
 window.addEventListener("mousedown", function (args) {
     for (var i = 0; i < 10; i++) {
         for (var j = 0; j < 10; j++) {
             if (isThisPointInTheSquare(mishkaX, mishkaY, 40 * j, 40 * i, 40, 40)) {
                 if (map[i][j] == 0 || (whoseBallsAreThese[i][j] == currentPlayer)) {
+                    for (var k = 0; k < 10; k++) {
+                        for (var l = 0;l < 10; l++) {
+                            oldmap[k][l] = map[k][l];
+                            oldWhoseBalls[k][l] = whoseBallsAreThese[k][l];
+                        }
+                    }
+                    napravilLiSumUndo = false;
                     map[i][j]++;
                     whoseBallsAreThese[i][j] = currentPlayer;
                     if (map[i][j] == neighbors[i][j]) {
@@ -88,10 +142,6 @@ window.addEventListener("mousedown", function (args) {
     }
 }, false);
 
-function update() {
-    whoseBallsAreThese[6][3] = 1;
-	setTimeout(update, 10);
-}
 function drawCell(i, j, color) {
     var x = 40 * j;
     var y = 40 * i;
@@ -136,5 +186,4 @@ function draw() {	//specialna funkcia v koqto shte pishem koda za risuvane. Shte
     context.stroke();
 }
 
-update();
 draw();
